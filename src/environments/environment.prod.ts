ea0ac9155e3775680869e5086f708518b0c10b60
nader medhet <nadermedhet142@gmail.com>
2019-01-21 17:13:08 +0200

    design fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true
 };
+import { urlSettings } from '../app/services/settings/urlSettings';
+urlSettings.Settings = {
+  apiEndPoint: 'api/v1',
+  apiHost: 'backend.cambridgeuniv.uk',
+  apiProtocol: 'http'
+}
